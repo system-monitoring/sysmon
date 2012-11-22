1769668652a0153008fc6ce77b3cf80a2b3bfaeb
Ishanka Anuradha Ranasooriya <ishanka@thinkcube.com>
2012-11-22 11:04:53 +0530

    Add File to GIT

---
+++ Deamon/settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+DEBUG = False
+
+GENERIC_ERROR_CODE = '100'
+NETWORK_GENERIC_ERROR_CODE = '400'
+NETWORK_WRONGURL_ERROR_CODE = '410'
+DB_GENERIC_ERROR_CODE = '500'
+IO_GENERIC_ERROR_CODE = '300'
+
+LOG_PROGRAM = 'tenant-monitordaemon'
+
+shells = ['Service']
+interval = 180
